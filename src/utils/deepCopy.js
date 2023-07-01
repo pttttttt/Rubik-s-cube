59be5cdaaa4ec11f482d844a7bda6f9210375434
pt <2779655209@qq.com>
2023-07-01 18:50:56 +0800

    优化逻辑

--- src/utils/deepCopy.js
+++ src/utils/deepCopy.js
@@ -1,15 +1,25 @@
-export default function deepCopy(oldobj, newobj) {
-  for (let k in oldobj) {
-    const item = oldobj[k]
-    if (item instanceof Array) {
-      newobj[k] = []
-      deepCopy(item, newobj[k])
-    } else if (item instanceof Object) {
-      newobj[k] = {};
-      deepCopy(item, newobj[k])
-    } else {
-      newobj[k] = item
+/**
+ * 深克隆
+ * @param {*} obj 需克隆的参数
+ * @returns 新对象
+ */
+export default function deepCopy (obj) {
+  let type = {}
+  if (obj instanceof Array) type = []
+  return copy(obj, type)
+  function copy (oldobj, newobj) {
+    for (let k in oldobj) {
+      const item = oldobj[k]
+      if (item instanceof Array) {
+        newobj[k] = []
+        copy(item, newobj[k])
+      } else if (item instanceof Object) {
+        newobj[k] = {};
+        copy(item, newobj[k])
+      } else {
+        newobj[k] = item
+      }
     }
+    return newobj
   }
-  return newobj
 }

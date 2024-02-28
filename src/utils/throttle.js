9e1450187b6644893244075259a1389c2f60bf8e
pt <2779655209@qq.com>
2024-02-28 11:56:42 +0800

    优化节流函数

--- src/utils/throttle.js
+++ src/utils/throttle.js
@@ -2,15 +2,15 @@
  * 防抖
  * @param {function} fn 需防抖的函数
  * @param {number} delay 延时 默认500毫秒
+ * @param {object} that fn指向的对象
  * @returns {function}
  */
-export default function throttle (fn, delay = 500) {
+export default function throttle (fn, delay = 500, that = this) {
   let flag = true
-  return function(...args) {
+  return function() {
     if (flag) {
       flag = false
-      // fn.apply(this, ...args)
-      fn(...args)
+      fn.call(that, ...arguments)
       setTimeout(() => flag = true, delay)
     }
   }

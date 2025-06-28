023ac1cc9c2ad88dc8f4122be8a7df63ccfb4201
Rick <2779655209@qq.com>
2025-06-28 18:02:34 +0800

    合并工具函数至同个文件

--- src/utils/pasteTextToClipboard.js
+++
@@ -1,8 +0,0 @@
-/**
- * 将传入的字符串复制到剪切板
- * @param {string} text 需粘贴到剪贴板的字符串
- * @returns {Promise} promise
- */
-export default function pasteTextToClipboard (text) {
-  return navigator.clipboard.writeText(text)
-}
---
+++ src/utils/util.js
@@ -0,0 +1,64 @@
+/**
+ * 深克隆
+ * @param {*} obj 需克隆的对象
+ * @returns 新对象
+ */
+export function deepCopy(obj) {
+  let type = {}
+  if (obj instanceof Array) type = []
+  return copy(obj, type)
+  function copy(oldobj, newobj) {
+    for (let k in oldobj) {
+      const item = oldobj[k]
+      if (item instanceof Array) {
+        newobj[k] = []
+        copy(item, newobj[k])
+      } else if (item instanceof Object) {
+        newobj[k] = {};
+        copy(item, newobj[k])
+      } else {
+        newobj[k] = item
+      }
+    }
+    return newobj
+  }
+}
+/**
+ * 节流
+ * @param {function} fn 需节流的函数
+ * @param {number} delay 延时 默认500毫秒
+ * @param {object} that fn指向的对象
+ * @returns {function}
+ */
+export function throttle(fn, delay = 500, that = this) {
+  let flag = true
+  return function () {
+    if (flag) {
+      flag = false
+      fn.call(that, ...arguments)
+      setTimeout(() => flag = true, delay)
+    }
+  }
+}
+/**
+ * 将传入的字符串复制到剪切板
+ * @param {string} text 需粘贴到剪贴板的字符串
+ * @returns {Promise} promise
+ */
+export function pasteTextToClipboard(text) {
+  return navigator.clipboard.writeText(text)
+}
+/**
+ * rbg字符串转十六进制
+ * @param {string} rgbStr 需转换的rbg字符串
+ * @returns 十六进制字符串
+ */
+export function rgbToHexadecimal(rgbStr) {
+  // 将RGB字符串拆分为三个数值
+  let rgbArray = rgbStr.substring(4, rgbStr.length - 1).split(',')
+  // 将每个数值转换为16进制，并拼接起来
+  let hexadecimal = "#" + Number(rgbArray[0]).toString(16).padStart(2, '0') +
+    Number(rgbArray[1]).toString(16).padStart(2, '0') +
+    Number(rgbArray[2]).toString(16).padStart(2, '0')
+  return hexadecimal
+}
